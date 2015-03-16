70f683be9f8a77dcd5c722272892b580955f04dc
Tauno Hanni <taunoh@gmail.com>
2015-03-16 11:17:43 +0200

    Create jquery.px.readmore.js

---
+++ jquery.px.readmore.js
@@ -0,0 +1,55 @@
+(function($) {
+
+    $.fn.readmore = function(options) {
+
+        var defaults = {
+            maxHeight: 200,
+            btnClasses: 'button link',
+            helperConClasses: 'px-readmore-helper',
+            toggleConClasses: 'px-readmore-toggle'
+        };
+
+        var settings = $.extend({}, defaults, options);
+
+        var toggle = function(e) {
+            var $this = $(this);
+            var $con = $this.closest('[data-readmore]');
+
+            if ($con.hasClass('open')) {
+                $con.removeClass('open').attr('aria-expanded', false);;
+                $this.text($con.data('readmore'));
+                return;
+            }
+
+            $con.addClass('open').attr('aria-expanded', true);
+            $this.text($con.data('readmore-less'));
+
+        };
+
+        return this.each(function() {
+            var $el = $(this);
+            var token = 'oi-' + Math.random().toString(36).substr(2);
+
+            if ($el.height() < settings.maxHeight)
+                return;
+
+            $el.addClass('has-readmore').attr('aria-expanded', false).attr('id', token);
+            $el.wrapInner(function() {
+                return '<div class="' + settings.helperConClasses + '"></div>';
+            });
+
+            $el.append(
+                $('<div />')
+                .addClass(settings.toggleConClasses)
+                .append(
+                    $('<button type="button" class="' + settings.btnClasses + '" aria-controls="' + token + '">' + $el.data('readmore') + '</button>').on('click.readmore', toggle)
+                )
+            );
+        });
+    };
+
+    $(function() {
+        $('[data-readmore]').readmore();
+    });
+
+}(jQuery));
